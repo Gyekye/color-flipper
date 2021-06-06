d87b7a51136bf64115b7d6a9386d347af277cb92
Mr. Richmond Gyekye <mrgyekye740@gmail.com>
2021-06-06 23:55:45 +0000

    proj-done

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green','red','rgba(21,222,34,45)','#f2d4f5','#DAF7A6','#D35400','LightCoral','LightSalmon'];
+
+// targeting elements
+
+let btn = document.getElementById('btn');
+let color = document.querySelector('.color');
+
+// event listener
+btn.addEventListener('click',function(){
+    let randomNumber =  getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+}
